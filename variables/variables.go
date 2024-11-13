b2e9f52cfa391a23e9f99cb5280468e83a38e5fe
flaviano-faria <flsilv89@yahoo.com.br>
2024-11-13 19:59:07 -0300

    print variable value and type

--- variables/variables.go
+++ variables/variables.go
@@ -3,8 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	x:=10
-	y:="hello world"
-	
+	x := 10
+	y := "hello world"
+
 	fmt.Println(x, y)
+
+	//print variable value and type
+	fmt.Printf("x: %v, %T\n", x, x)
 }
